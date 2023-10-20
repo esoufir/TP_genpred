90bcdb963b1812c10abf52bda4101d877cac9251
esoufir <emmas@Mitochondrie>
2023-10-20 15:28:08 +0200

    to_main

--- gpred/gpred.py
+++ gpred/gpred.py
@@ -1,4 +1,5 @@
 import argparse
+#from msilib import sequence
 import sys
 import os
 import csv
@@ -6,7 +7,8 @@ import re
 from re import Pattern
 from pathlib import Path
 from typing import List, Union, Optional
-
+import textwrap
+from operator import itemgetter
 
 def isfile(path: str) -> Path:  # pragma: no cover
     """Check if path is an existing file.
@@ -60,7 +62,12 @@ def read_fasta(fasta_file: Path) -> str:
     :param fasta_file: (Path) Path to the fasta file.
     :return: (str) Sequence from the genome. 
     """
-    pass
+    seq=""
+    with open(fasta_file, "r") as f_in:
+        for line in f_in:
+            if not line.startswith(">"):
+                seq+=line.strip()
+    return seq    
 
 
 def find_start(start_regex: Pattern, sequence: str, start: int, stop: int) -> Union[int, None]:
@@ -72,7 +79,11 @@ def find_start(start_regex: Pattern, sequence: str, start: int, stop: int) -> Un
     :param stop: (int) Stop position of the research
     :return: (int) If exist, position of the start codon. Otherwise None. 
     """
-    pass
+    matchs = start_regex.search(sequence, start, stop)
+    ind_start = None
+    if matchs:
+        ind_start = matchs.start(0)
+    return ind_start
 
 
 def find_stop(stop_regex: Pattern, sequence: str, start: int) -> Union[int, None]:
@@ -83,7 +94,14 @@ def find_stop(stop_regex: Pattern, sequence: str, start: int) -> Union[int, None
     :param start: (int) Start position of the research
     :return: (int) If exist, position of the stop codon. Otherwise None. 
     """
-    pass
+    matchs = stop_regex.finditer(sequence, start)
+    ind_stop = None
+    for match_found in matchs:
+        if (match_found.start(0) - start) % 3 == 0 :
+            ind_stop = match_found.start(0)
+            break
+    return ind_stop
+    
 
 
 def has_shine_dalgarno(shine_regex: Pattern, sequence: str, start: int, max_shine_dalgarno_distance: int) -> bool:
@@ -95,7 +113,12 @@ def has_shine_dalgarno(shine_regex: Pattern, sequence: str, start: int, max_shin
     :param max_shine_dalgarno_distance: (int) Maximum distance of the shine dalgarno to the start position
     :return: (boolean) true -> has a shine dalgarno upstream to the gene, false -> no
     """
-    pass
+    if start-max_shine_dalgarno_distance < 0:
+        return False
+    match = shine_regex.search(sequence, start-max_shine_dalgarno_distance, start - 5)
+    if match:
+        return True
+    return False
 
 
 def predict_genes(sequence: str, start_regex: Pattern, stop_regex: Pattern, shine_regex: Pattern, 
@@ -111,7 +134,31 @@ def predict_genes(sequence: str, start_regex: Pattern, stop_regex: Pattern, shin
     :param min_gap: (int) Minimum distance between two genes.
     :return: (list) List of [start, stop] position of each predicted genes.
     """
-    pass
+    position_courante = 0
+    gene_list = []
+
+    while len(sequence) - position_courante >= min_gap:
+        position_courante = find_start(start_regex, sequence, position_courante, len(sequence))
+        if position_courante:
+            # Finding codon stop
+            stop = find_stop(stop_regex, sequence, position_courante)
+            if stop:
+                gene_len = stop+2-position_courante+1
+
+                if gene_len >= min_gene_len:
+                    #If found shine dalgarno seq : 
+                    if has_shine_dalgarno(shine_regex, sequence, position_courante, max_shine_dalgarno_distance):
+                            gene_list.append([position_courante+1, stop+3])
+                            position_courante = stop + 3 + min_gap
+                    else :
+                        position_courante = position_courante + 1
+                else:
+                    position_courante = position_courante + 1
+            else:
+                position_courante = position_courante + 1
+        else:
+            break
+    return gene_list
 
 
 def write_genes_pos(predicted_genes_file: Path, probable_genes: List[List[int]]) -> None:
@@ -144,12 +191,12 @@ def write_genes(fasta_file: Path, sequence: str, probable_genes: List[List[int]]
             for i,gene_pos in enumerate(probable_genes):
                 fasta.write(">gene_{0}{1}{2}{1}".format(
                     i+1, os.linesep, 
-                    fill(sequence[gene_pos[0]-1:gene_pos[1]])))
+                    textwrap.fill(sequence[gene_pos[0]-1:gene_pos[1]])))
             i = i+1
             for j,gene_pos in enumerate(probable_genes_comp):
                 fasta.write(">gene_{0}{1}{2}{1}".format(
                             i+1+j, os.linesep,
-                            fill(sequence_rc[gene_pos[0]-1:gene_pos[1]])))
+                            textwrap.fill(sequence_rc[gene_pos[0]-1:gene_pos[1]])))
     except IOError:
         sys.exit("Error cannot open {}".format(fasta_file))
 
@@ -182,17 +229,23 @@ def main() -> None: # pragma: no cover
     shine_regex = re.compile('A?G?GAGG|GGAG|GG.{1}GG')
     # Arguments
     args = get_arguments()
-    # Let us do magic in 5' to 3'
+    seq = read_fasta(args.genome_file)
     
+
+    # Let us do magic in 5' to 3'
+    gene_predict = predict_genes(seq, start_regex,stop_regex,shine_regex, args.min_gene_len, args.max_shine_dalgarno_distance,args.min_gap)
     # Don't forget to uncomment !!!
     # Call these function in the order that you want
     # We reverse and complement
-    #sequence_rc = reverse_complement(sequence)
+    sequence_rc = reverse_complement(seq)
     # Call to output functions
-    #write_genes_pos(args.predicted_genes_file, probable_genes)
-    #write_genes(args.fasta_file, sequence, probable_genes, sequence_rc, probable_genes_comp)
+    gene_predict_reverse = predict_genes(sequence_rc, start_regex,stop_regex,shine_regex, args.min_gene_len, args.max_shine_dalgarno_distance,args.min_gap)
 
+    all_genes = gene_predict + gene_predict_reverse
 
+    all_genes_sorted = sorted(all_genes, key=lambda x: x[0])
+
+    write_genes_pos(args.predicted_genes_file, all_genes_sorted)
 
 if __name__ == '__main__':
     main()
